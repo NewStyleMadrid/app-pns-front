d859647a0f9fe8b66e515c574b28de356f9945c8
PepeLeob <pepeleob@gmail.com>
2021-05-05 14:02:59 +0200

    Generando editor de galería en el menú

---
+++ src/app/components/home/nuevo-home/nuevo-corte.component.ts
@@ -0,0 +1,55 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ImagenService } from 'src/app/service/imagen.service';
+
+@Component({
+  selector: 'app-nuevo-corte',
+  templateUrl: './nuevo-corte.component.html',
+  styleUrls: ['./nuevo-corte.component.css']
+})
+export class NuevoCorteComponent implements OnInit {
+
+  @ViewChild('imagenInputFile', {static: false}) imagenFile: ElementRef;
+
+  imagen: File;
+  imagenMin: File;
+
+  constructor(
+    private imgService: ImagenService, 
+    private router: Router, 
+    private spinner: NgxSpinnerService) { }
+
+  ngOnInit(): void {
+  }
+
+  onFileChange(event) {
+    this.imagen = event.target.files[0];
+    const fr = new FileReader();
+    fr.onload = (evento: any) => {
+      this.imagenMin = evento.target.result;
+    };
+    fr.readAsDataURL(this.imagen);
+  }
+
+  onUpload(): void {
+    this.spinner.show();
+    this.imgService.upload(this.imagen).subscribe(
+      data => {
+        this.spinner.hide();
+        this.router.navigate(['/lista-diseños']);
+      },
+      err => {
+        alert(err.error.mensaje);
+        this.spinner.hide();
+        this.reset();
+      }
+    );
+  }
+
+  reset(): void {
+    this.imagen = null;
+    this.imagenMin = null;
+    this.imagenFile.nativeElement.value = '';
+  }
+}
