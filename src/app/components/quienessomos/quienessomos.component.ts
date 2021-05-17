ac31eb73a41981319012b7218242ea2044c5f4e1
unknown <peluqueria.newstyle.madrid@gmail.com>
2021-05-17 13:34:40 +0200

    update quienessomos

--- src/app/components/quienessomos/quienessomos.component.ts
+++ src/app/components/quienessomos/quienessomos.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuienessomosComponent implements OnInit {
 
+   // Nivel de zoom de maps
+   zoom: number = 15;
+
+   // Latitud y Longitud de Madrid
+   lat: number = 40.4167;
+   lng: number = -3.70325;
+ 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  markers: marker[] = [
+    {
+      lat: 40.4167,
+      lng: -3.70325,
+      label: 'A',
+      draggable: true
+    }
+    //Aqui podemos añadir más por si se extiende la franquicia
+  ]
+
 }
+
+// Interfaz para la seguridad.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
